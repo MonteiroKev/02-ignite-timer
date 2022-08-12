6186c854c07b9954d1be02685ce851e8d7dd5b51
MonteiroKev <monteirokevyn@gmail.com>
2022-08-11 22:19:36 -0300

    feat: Formulários - Resetando formulário

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,9 +1,88 @@
-import { HistoryContainer } from "./style";
+import { HistoryContainer, HistoryList, Status } from "./style";
 
 export function History() {
   return (
     <HistoryContainer>
-      <h1>History</h1>
+      <h1>Meu histórico</h1>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duração</th>
+              <th>Início</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca 2 meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca 2 meses</td>
+              <td>
+                <Status statusColor="yellow">Em andamento</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca 2 meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca 2 meses</td>
+              <td>
+                <Status statusColor="red">Interrompido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+            <tr>
+              <td>tarefa</td>
+              <td>20 minutos</td>
+              <td>Há cerca 2 meses</td>
+              <td>
+                <Status statusColor="green">Concluido</Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
     </HistoryContainer>
   );
 }
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,16 +9,47 @@ import {
   TaskInput,
 } from "./styles";
 
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as zod from "zod";
+
+const newCycleFormValidationSchema = zod.object({
+  task: zod.string().min(1, "Informe a tarefa"),
+  minutesAmount: zod
+    .number()
+    .min(5, "O ciclo precisa ser no mínimo 5 minutos")
+    .max(60, "O ciclo precisa ser no máximo 60 minutos"),
+});
+
+type NewCycleFormDate = zod.infer<typeof newCycleFormValidationSchema>; // pegando a tipagem do newCycleFormValidationSchema
+
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormDate>({
+    resolver: zodResolver(newCycleFormValidationSchema),
+    defaultValues: {
+      task: "",
+      minutesAmount: 0,
+    },
+  });
+
+  function handleCreateNewCycle(data: NewCycleFormDate) {
+    console.log(data);
+    reset();
+  }
+
+  const task = watch("task");
+  const isSubmitDisabled = !task;
+
   return (
     <HomeContainer>
-      <form action="">
+      <form action="" onSubmit={handleSubmit(handleCreateNewCycle)}>
         <FormContainer>
           <label htmlFor="task">Vou trabalhar em</label>
           <TaskInput
             id="task"
             list="task-suggestions"
             placeholder="Dê um nome para o seu projeto"
+            {...register("task")}
           />
 
           <datalist id="task-suggestions">
@@ -35,7 +66,8 @@ export function Home() {
             placeholder="00"
             step={5}
             min={5}
-            max={60}
+            // max={60}
+            {...register("minutesAmount", { valueAsNumber: true })}
           />
 
           <span>minutos.</span>
@@ -49,7 +81,7 @@ export function Home() {
           <span>0</span>
         </CountDownContainer>
 
-        <StartCountDownButton disabled type="submit">
+        <StartCountDownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Começar
         </StartCountDownButton>
