4ea2225ce7b511fa95dc56913ef9061c17542317
MonteiroKev <monteirokevyn@gmail.com>
2022-08-19 14:41:19 -0300

    feat: Ciclo Completo

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -21,7 +21,7 @@ const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "Informe a tarefa"),
   minutesAmount: zod
     .number()
-    .min(5, "O ciclo precisa ser no mínimo 5 minutos")
+    .min(1, "O ciclo precisa ser no mínimo 5 minutos")
     .max(60, "O ciclo precisa ser no máximo 60 minutos"),
 });
 
@@ -33,6 +33,7 @@ interface Cycle {
   minutesAmount: number;
   startDate: Date;
   interruptedDate?: Date;
+  finishedDate?: Date;
 }
 
 export function Home() {
@@ -49,22 +50,42 @@ export function Home() {
   });
 
   const activeCycle = cycles.find((cycles) => cycles.id === activeCycleId);
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
 
   useEffect(() => {
     let interval: number;
 
     if (activeCycle) {
       interval = setInterval(() => {
-        setAmountSecondsPassed(
-          differenceInSeconds(new Date(), activeCycle.startDate)
+        const secondsDifference = differenceInSeconds(
+          new Date(),
+          activeCycle.startDate
         );
+
+        if (secondsDifference >= totalSeconds) {
+          setCycles((state) =>
+            state.map((cycle) => {
+              if (cycle.id == activeCycleId) {
+                return { ...cycle, finishedDate: new Date() };
+              } else {
+                return cycle;
+              }
+            })
+          );
+
+          setAmountSecondsPassed(totalSeconds);
+
+          clearInterval(interval);
+        } else {
+          setAmountSecondsPassed(secondsDifference);
+        }
       }, 1000);
     }
 
     return () => {
       clearInterval(interval);
     };
-  }, [activeCycle]);
+  }, [activeCycle, totalSeconds, activeCycleId]);
 
   function handleCreateNewCycle(data: NewCycleFormDate) {
     const id = String(new Date().getTime());
@@ -84,8 +105,8 @@ export function Home() {
   }
 
   function handleInterruptCycle() {
-    setCycles(
-      cycles.map((cycle) => {
+    setCycles((state) =>
+      state.map((cycle) => {
         if (cycle.id == activeCycleId) {
           return { ...cycle, interruptedDate: new Date() };
         } else {
@@ -93,10 +114,10 @@ export function Home() {
         }
       })
     );
+
     setActiveCycleId(null);
   }
 
-  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
 
   const minutesAmount = Math.floor(currentSeconds / 60);
@@ -142,7 +163,7 @@ export function Home() {
             id="minutesAmount"
             placeholder="00"
             step={5}
-            min={5}
+            min={1}
             // max={60}
             {...register("minutesAmount", { valueAsNumber: true })}
             disabled={!!activeCycle}
